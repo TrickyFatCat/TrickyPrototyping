a654bb1ab2149d3b0fc8c675648119f5c00cc6d0
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-17 22:59:07 +0300

    Implement EntityResource logic

--- Source/TrickyPrototyping/Public/Objects/EntityResource.h
+++ Source/TrickyPrototyping/Public/Objects/EntityResource.h
@@ -10,6 +10,9 @@
  * This object contains logic for basic entity resources such as Health Points, Armor, Stamina, etc.
  */
 
+DECLARE_MULTICAST_DELEGATE_TwoParams(FOnValueChangedSignature, float, float);
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnValueMaxChangedSignature, float);
+
 USTRUCT(BlueprintType)
 struct FResourceAutoChangeData
 {
@@ -49,8 +52,60 @@ struct FResourceData
 	FResourceAutoChangeData AutoDecreaseData;
 };
 
-UCLASS()
+UCLASS(Abstract)
 class TRICKYPROTOTYPING_API UEntityResource : public UBaseObject
 {
 	GENERATED_BODY()
+	// General
+public:
+	void SetResourceData(const FResourceData& NewResourceData);
+	FResourceData GetResourceData() const { return ResourceData; }
+
+	// Value
+	void SetValue(const float NewValue);
+	float GetValue() const { return ResourceData.Value; }
+	void DecreaseValue(const float DeltaValue);
+	void IncreaseValue(const float DeltaValue, const bool bClampToMax = true);
+	float GetNormalizedValue() const { return ResourceData.Value / ResourceData.ValueMax; }
+
+	// ValueMax
+	void SetValueMax(const float NewValue, const bool bClampValue = false);
+	float GetValueMax() const { return ResourceData.ValueMax; }
+	void DecreaseValueMax(const float DeltaValue, const bool bClampValue = true);
+	void IncreaseValueMax(const float DeltaValue, const bool bClampValue = false);
+
+	// Delegates
+	FOnValueChangedSignature OnValueChanged;
+	FOnValueMaxChangedSignature OnValueMaxChanged;
+protected:
+	FResourceData ResourceData;
+	void StopTimer(FTimerHandle& TimerHandle) const;
+
+	// Auto increase
+public:
+	void ProcessAutoIncrease();
+	void StartAutoIncrease();
+	void StopAutoIncrease();
+	void SetAutoIncreaseEnabled(const bool bIsEnabled);
+	void SetAutoIncreaseFrequency(const float NewFrequency);
+	void SetAutoIncreaseValue(const float NewValue);
+	void SetAutoIncreaseThreshold(const float NewThreshold);
+	void SetAutoIncreaseStartDelay(const float NewDelay);
+
+protected:
+	FTimerHandle AutoIncreaseTimerHandle;
+	
+	// Auto decrease
+public:
+	void ProcessAutoDecrease();
+	void StartAutoDecrease();
+	void StopAutoDecrease();
+	void SetAutoDecreaseEnabled(const bool bIsEnabled);
+	void SetAutoDecreaseFrequency(const float NewFrequency);
+	void SetAutoDecreaseValue(const float NewValue);
+	void SetAutoDecreaseThreshold(const float NewThreshold);
+	void SetAutoDecreaseStartDelay(const float NewDelay);
+	
+private:
+	FTimerHandle AutoDecreaseTimerHandle;
 };
