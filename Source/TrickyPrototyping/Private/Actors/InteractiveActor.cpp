a820716c664236a69bbce28c6fc2c8b130a80f93
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-23 22:41:51 +0300

    Implement interactive actor logic

--- Source/TrickyPrototyping/Private/Actors/InteractiveActor.cpp
+++ Source/TrickyPrototyping/Private/Actors/InteractiveActor.cpp
@@ -55,17 +55,37 @@ void AInteractiveActor::SetAnimationDuration(const float Value)
 	CalculatePlayRate();
 }
 
-void AInteractiveActor::StartAnimation()
+void AInteractiveActor::SetAnimationProgress(const float Progress)
 {
-	if (!AnimationCurve) return;
-	
-	AnimationTimeline->PlayFromStart();
+	if (Progress < 0.f || Progress > 1.f) return;
+
+	AnimateTransform(Progress);
 }
 
-void AInteractiveActor::RevertAnimation()
+void AInteractiveActor::StartAnimation()
 {
-	if (!AnimationCurve) return;
-	
+	if (!AnimationCurve || GetAnimationDuration() <= 0.f) return;
+
+	SetTargetState();
+
+	switch (StateTarget)
+	{
+	case EInteractiveActorState::Opened:
+		AnimationTimeline->PlayFromStart();
+		break;
+	case EInteractiveActorState::Closed:
+		AnimationTimeline->ReverseFromEnd();
+		break;
+	}
+}
+
+void AInteractiveActor::ReverseAnimation()
+{
+	if (!AnimationCurve || !GetIsRevertible() || GetAnimationDuration() <= 0.f) return;
+
+	StateTarget = StateTarget == EInteractiveActorState::Closed
+		              ? EInteractiveActorState::Opened
+		              : EInteractiveActorState::Closed;
 	AnimationTimeline->Reverse();
 }
 
@@ -76,7 +96,7 @@ void AInteractiveActor::StopAnimation()
 
 void AInteractiveActor::FinishAnimation()
 {
-	
+	SetState(StateTarget);
 }
 
 void AInteractiveActor::CalculatePlayRate()
@@ -95,11 +115,110 @@ void AInteractiveActor::AnimateTransform(const float AnimationProgress)
 
 		NewTransform.SetLocation(NewTransform.GetLocation() + TargetTransform.GetLocation() * AnimationProgress);
 
-		FQuat NewRotation = FRotator(NewTransform.GetRotation().Rotator() + TargetTransform.GetRotation().Rotator() * AnimationProgress).Quaternion();
+		FQuat NewRotation = FRotator(
+				NewTransform.GetRotation().Rotator() + TargetTransform.GetRotation().Rotator() * AnimationProgress).
+			Quaternion();
 		NewTransform.SetRotation(NewRotation);
 
 		NewTransform.SetScale3D(NewTransform.GetScale3D() + TargetTransform.GetScale3D() * AnimationProgress);
 
 		AnimatedComponents.Array()[i]->SetRelativeTransform(NewTransform);
 	}
+}
+
+bool AInteractiveActor::CanBeReversed() const
+{
+	return IsStateCurrent(EInteractiveActorState::Transition) && bIsRevertible;
+}
+
+void AInteractiveActor::SetState(const EInteractiveActorState NewState)
+{
+	if (!IsStateCurrent(NewState)) return;
+
+	StatePrevious = StateCurrent;
+	StateCurrent = NewState;
+	OnActorChangedState.Broadcast(NewState);
+	OnStateChanged(NewState);
+}
+
+void AInteractiveActor::SetTargetState()
+{
+	StateTarget = StateCurrent == EInteractiveActorState::Closed
+		              ? EInteractiveActorState::Opened
+		              : EInteractiveActorState::Closed;
+}
+
+bool AInteractiveActor::CanStartAnimation() const
+{
+	return !IsStateCurrent(EInteractiveActorState::Locked) || !IsStateCurrent(EInteractiveActorState::Disabled);
+}
+
+void AInteractiveActor::Open() 
+{
+	if (IsStateCurrent(EInteractiveActorState::Opened) || !CanStartAnimation()) return;
+
+	if (CanBeReversed() && StateTarget == EInteractiveActorState::Closed)
+	{
+		ReverseAnimation();
+		return;
+	}
+
+	if (AnimationDuration <= 0.f)
+	{
+		SetTargetState();
+		AnimateTransform(1.f);
+		FinishAnimation();
+		return;
+	}
+
+	StartAnimation();
+}
+
+void AInteractiveActor::Close()
+{
+	if (IsStateCurrent(EInteractiveActorState::Closed) || !CanStartAnimation()) return;
+
+	if (CanBeReversed() && StateTarget == EInteractiveActorState::Opened)
+	{
+		ReverseAnimation();
+		return;
+	}
+
+	if (AnimationDuration <= 0.f)
+	{
+		SetTargetState();
+		AnimateTransform(0.f);
+		FinishAnimation();
+		return;
+	}
+
+	StartAnimation();
+}
+
+void AInteractiveActor::Lock()
+{
+	if (!IsStateCurrent(EInteractiveActorState::Closed)) return;
+
+	SetState(EInteractiveActorState::Locked);
+}
+
+void AInteractiveActor::Unlock()
+{
+	if (!IsStateCurrent(EInteractiveActorState::Locked)) return;
+
+	SetState(EInteractiveActorState::Closed);
+}
+
+void AInteractiveActor::Enable()
+{
+	if (IsStateCurrent(EInteractiveActorState::Disabled)) return;
+
+	SetState(StatePrevious);
+}
+
+void AInteractiveActor::Disable()
+{
+	if (!IsStateCurrent(EInteractiveActorState::Transition)) return;
+
+	SetState(EInteractiveActorState::Disabled);
 }
--- Source/TrickyPrototyping/Public/Actors/InteractiveActor.h
+++ Source/TrickyPrototyping/Public/Actors/InteractiveActor.h
@@ -14,6 +14,18 @@ class UTimelineComponent;
  * It contains the basic logic for controlling animation for this objects.
  */
 
+UENUM()
+enum class EInteractiveActorState : uint8
+{
+	Opened,
+	Closed,
+	Locked,
+	Disabled,
+	Transition UMETA(Hidden)
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractiveActorChangedStateSignature, EInteractiveActorState, NewState);
+
 UCLASS()
 class TRICKYPROTOTYPING_API AInteractiveActor : public AActor
 {
@@ -42,24 +54,27 @@ public:
 	UFUNCTION(BlueprintSetter, Category="Animation")
 	void SetIsRevertible(const bool Value) { bIsRevertible = Value; }
 
+	UFUNCTION(BlueprintCallable, Category="Animation")
+	void SetAnimationProgress(const float Progress);
+
 protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category="Components")
 	UTimelineComponent* AnimationTimeline = nullptr;
-	
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation")
 	UCurveFloat* AnimationCurve = nullptr;
-	
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation")
 	TSet<USceneComponent*> AnimatedComponents;
-	
+
 	UPROPERTY()
 	TArray<FTransform> InitialTransforms;
-	
+
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Animation")
 	TArray<FTransform> TargetTransforms;
-	
+
 	virtual void StartAnimation();
-	virtual void RevertAnimation();
+	virtual void ReverseAnimation();
 	virtual void StopAnimation();
 	virtual void FinishAnimation();
 
@@ -70,17 +85,70 @@ private:
 		Category="Animation",
 		meta=(AllowPrivateAccess="true"))
 	float AnimationDuration = 1.f;
-	
+
 	UPROPERTY(EditDefaultsOnly,
 		BlueprintGetter=GetIsRevertible,
 		BlueprintSetter=SetIsRevertible,
 		Category="Animation",
 		meta=(AllowPrivateAccess="true"))
 	bool bIsRevertible = false;
-	
+
 	UFUNCTION(BlueprintCallable, Category="Animation")
 	void CalculatePlayRate();
-	
+
 	UFUNCTION()
 	void AnimateTransform(const float AnimationProgress);
+
+	bool CanBeReversed() const;
+
+	// States
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="States")
+	EInteractiveActorState StateInitial = EInteractiveActorState::Closed;
+
+	UFUNCTION(BlueprintCallable, Category="States")
+	EInteractiveActorState GetStateCurrent() const { return StateCurrent; }
+
+	UPROPERTY(BlueprintAssignable, Category="States")
+	FOnInteractiveActorChangedStateSignature OnActorChangedState;
+
+	UFUNCTION(BlueprintCallable, Category="States")
+	bool IsStateCurrent(const EInteractiveActorState DoorState) const { return StateCurrent == DoorState; }
+
+	UFUNCTION(BlueprintImplementableEvent, Category="States")
+	void OnStateChanged(const EInteractiveActorState NewState);
+
+protected:
+	UFUNCTION(BlueprintCallable, Category="States")
+	void SetState(const EInteractiveActorState NewState);
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="States", meta=(AllowPrivateAccess="true"))
+	EInteractiveActorState StateCurrent = EInteractiveActorState::Closed;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="States", meta=(AllowPrivateAccess="true"))
+	EInteractiveActorState StatePrevious = EInteractiveActorState::Transition;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="States", meta=(AllowPrivateAccess="true"))
+	EInteractiveActorState StateTarget = EInteractiveActorState::Opened;
+
+	void SetTargetState();
+	bool CanStartAnimation() const;
+
+	// Actions
+public:
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Open();
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Close();
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Lock();
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Unlock();
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Enable();
+	UFUNCTION(BlueprintCallable, Category="Actions")
+	virtual void Disable();
+	
+protected:
+private:
 };
