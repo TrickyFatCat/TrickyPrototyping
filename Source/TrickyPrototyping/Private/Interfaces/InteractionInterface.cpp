59824174ce0ea641576c0c09ef2fd3de37be01ec
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-24 23:57:47 +0300

    Create interaction interface

---
+++ Source/TrickyPrototyping/Private/Interfaces/InteractionInterface.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+
+#include "Interfaces/InteractionInterface.h"
+
+// Add default functionality here for any IInteractionInterface functions that are not pure virtual.
