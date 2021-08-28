1b400cf1542553d0a1d4c26eadc8e1b02c630c18
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-08-28 18:30:25 +0300

    Create base user widget with hide and show animations

---
+++ Source/TrickyPrototyping/Public/UserInterface/BaseUserWidget.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2021 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "BaseUserWidget.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnShowedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnHiddenSignature);
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYPROTOTYPING_API UBaseUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void Show();
+	void Hide();
+
+	UPROPERTY(BlueprintAssignable, Category="Animation")
+	FOnShowedSignature OnShowed;
+	UPROPERTY(BlueprintAssignable, Category="Animation")
+	FOnHiddenSignature OnHidden;
+
+protected:
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	UWidgetAnimation* ShowAnimation = nullptr;
+	
+	UPROPERTY(Transient, meta=(BindWidgetAnim))
+	UWidgetAnimation* HideAnimation = nullptr;
+
+	virtual void OnAnimationFinished_Implementation(const UWidgetAnimation* Animation) override;
+};
