8780a4cca6b3adc0ae3ea1325fd7bab145259747
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-09 17:41:58 +0300

    Update button base logic

--- Source/TrickyPrototyping/Private/Actors/InteractiveActors/ButtonBase.cpp
+++ Source/TrickyPrototyping/Private/Actors/InteractiveActors/ButtonBase.cpp
@@ -2,6 +2,17 @@
 
 
 #include "Actors/InteractiveActors/ButtonBase.h"
+#include "Components/TriggerComponents/InteractionCapsuleComponent.h"
+#include "Components/SceneComponent.h"
+
+AButtonBase::AButtonBase()
+{
+	ButtonRoot = CreateDefaultSubobject<USceneComponent>("ButtonRoot");
+	SetRootComponent(ButtonRoot);
+
+	ButtonTrigger = CreateDefaultSubobject<UInteractionCapsuleComponent>("ButtonTrigger");
+	ButtonTrigger->SetupAttachment(ButtonRoot);
+}
 
 void AButtonBase::BeginPlay()
 {
@@ -11,4 +22,61 @@ void AButtonBase::BeginPlay()
 void AButtonBase::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+}
+
+void AButtonBase::Disable()
+{
+	Super::Disable();
+
+	ButtonTrigger->SetIsEnabled(false);
+}
+
+void AButtonBase::Enable()
+{
+	if (bPressOnce) return;
+	
+	Super::Enable();
+	ButtonTrigger->SetIsEnabled(true);
+}
+
+void AButtonBase::StartAnimation()
+{
+	Super::StartAnimation();
+
+	ButtonTrigger->SetIsEnabled(false);
+}
+
+void AButtonBase::FinishAnimation()
+{
+	Super::FinishAnimation();
+
+	if (bPressOnce)
+	{
+		Disable();
+		return;
+	}
+
+	switch (ButtonBehaviour)
+	{
+	case EButtonBehaviour::Key:
+		if (IsStateCurrent(EInteractiveActorState::Closed))
+		{
+			if (!GetWorld()) return;
+
+			GetWorld()->GetTimerManager().SetTimer(AutoCloseDelayHandle,
+			                                       this,
+			                                       &AButtonBase::Close,
+			                                       KeyAutoCloseDelayDuration,
+			                                       false);
+		}
+		else
+		{
+			ButtonTrigger->SetIsEnabled(true);
+		}
+		break;
+
+	case EButtonBehaviour::Switch:
+		ButtonTrigger->SetIsEnabled(true);
+		break;
+	}
 }
--- Source/TrickyPrototyping/Public/Actors/InteractiveActors/ButtonBase.h
+++ Source/TrickyPrototyping/Public/Actors/InteractiveActors/ButtonBase.h
@@ -6,17 +6,59 @@
 #include "Actors/InteractiveActorBase.h"
 #include "ButtonBase.generated.h"
 
+class UInteractionCapsuleComponent;
+class USceneComponent;
+
 /**
  * A base button class. Use it for creating different buttons and levers
  */
+
+UENUM(BlueprintType)
+enum class EButtonBehaviour : uint8
+{
+	Key,
+	Switch
+};
+
 UCLASS()
 class TRICKYPROTOTYPING_API AButtonBase : public AInteractiveActorBase
 {
 	GENERATED_BODY()
 
+public:
+	AButtonBase();
+	
 protected:
 	virtual void BeginPlay() override;
 
 public:
 	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void Disable() override;
+	virtual void Enable() override;
+
+protected:
+	virtual void StartAnimation() override;
+	virtual void FinishAnimation() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
+	USceneComponent* ButtonRoot = nullptr;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
+	UInteractionCapsuleComponent* ButtonTrigger = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Button")
+	EButtonBehaviour ButtonBehaviour = EButtonBehaviour::Switch;
+	
+private:
+	UPROPERTY(EditAnywhere,
+		BlueprintReadWrite,
+		Category="Button",
+		meta=(AllowPrivateAccess="true", EditCondition="ButtonBehaviour == EButtonBehaviour::Key", ClampMin="0"))
+	float KeyAutoCloseDelayDuration = 1.f;
+
+	FTimerHandle AutoCloseDelayHandle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Button", meta=(AllowPrivateAccess="true"))
+	bool bPressOnce = false;
 };
