8956276cdf83b76c95a5b30995e1d4dced7dee6d
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-09-28 23:00:58 +0300

    Create TrickyUtils library

---
+++ Source/TrickyPrototyping/Private/Core/TrickyUtils.cpp
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2021 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+#pragma once
+
+#include "Core/TrickyUtils.h"
+#include "GameFramework/Character.h"
+
+bool FTrickyUtils::GetPlayerViewPoint(AActor* CharacterActor, FVector& ViewLocation, FRotator& ViewRotation)
+{
+	ACharacter* Character = Cast<ACharacter>(CharacterActor);
+
+	if (!Character) return false;
+
+	if (Character->IsPlayerControlled())
+	{
+		const APlayerController* Controller = Character->GetController<APlayerController>();
+
+		if (!Controller) return false;
+
+		Controller->GetPlayerViewPoint(ViewLocation, ViewRotation);
+	}
+
+	return true;
+	
+}
---
+++ Source/TrickyPrototyping/Public/Core/TrickyUtils.h
@@ -0,0 +1,9 @@
+﻿// Copyright (c) 2021 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+#pragma once
+
+class FTrickyUtils
+{
+public:
+	static bool GetPlayerViewPoint(AActor* CharacterActor, FVector& ViewLocation, FRotator& ViewRotation);
+};
