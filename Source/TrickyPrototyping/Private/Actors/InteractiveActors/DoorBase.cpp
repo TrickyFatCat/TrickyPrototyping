4dbc2deb1958a5dd7175acb2dce5b070e2d557d7
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-10-17 23:03:28 +0300

    Update base door

--- Source/TrickyPrototyping/Private/Actors/InteractiveActors/DoorBase.cpp
+++ Source/TrickyPrototyping/Private/Actors/InteractiveActors/DoorBase.cpp
@@ -2,13 +2,153 @@
 
 
 #include "Actors/InteractiveActors/DoorBase.h"
+#include "Components/TriggerComponents/InteractionBoxComponent.h"
+
+ADoorBase::ADoorBase()
+{
+	DoorTrigger = CreateDefaultSubobject<UInteractionBoxComponent>("DoorTrigger");
+	SetRootComponent(DoorTrigger);
+}
 
 void ADoorBase::BeginPlay()
 {
 	Super::BeginPlay();
+	SwitchDoorType();
+
+	DoorTrigger->OnComponentBeginOverlap.AddDynamic(this, &ADoorBase::OnTriggerBeginOverlap);
+	DoorTrigger->OnComponentEndOverlap.AddDynamic(this, &ADoorBase::OnTriggerEndOverlap);
 }
 
 void ADoorBase::Tick(float DeltaSeconds)
 {
 	Super::Tick(DeltaSeconds);
+}
+
+void ADoorBase::Disable()
+{
+	Super::Disable();
+
+	DoorTrigger->SetIsEnabled(false);
+}
+
+void ADoorBase::Enable()
+{
+	Super::Enable();
+
+	SwitchDoorType();
+}
+
+void ADoorBase::FinishAnimation()
+{
+	Super::FinishAnimation();
+
+	if (AutoCloseDelay <= 0.f || DoorTrigger->GetIsActorInside()) return;
+
+	StartAutoClose();
+}
+
+void ADoorBase::SetDoorType(const EDoorType Value)
+{
+	if (DoorType == Value) return;
+
+	DoorType = Value;
+	SwitchDoorType();
+}
+
+void ADoorBase::SwitchDoorType() const
+{
+	switch (DoorType)
+	{
+	case EDoorType::Auto:
+		DoorTrigger->SetIsNormalTrigger(true);
+		break;
+
+	case EDoorType::Interactive:
+		DoorTrigger->SetIsNormalTrigger(false);
+		DoorTrigger->bRequireLineOfSight = bRequireLineOfSight;
+		break;
+
+	case EDoorType::Manual:
+		DoorTrigger->SetIsEnabled(false);
+		break;
+	}
+}
+
+void ADoorBase::OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+                                      AActor* OtherActor,
+                                      UPrimitiveComponent* OtherComp,
+                                      int32 OtherBodyIndex,
+                                      bool bFromSweep,
+                                      const FHitResult& SweepResult)
+{
+	if (AutoCloseDelay > 0.f && GetStateCurrent() == EInteractiveActorState::Opened)
+	{
+		StopAutoClose();
+		return;
+	}
+	
+	switch (DoorType)
+	{
+	case EDoorType::Auto:
+		if (GetStateCurrent() != EInteractiveActorState::Closed) return;
+		
+		Open();
+		break;
+
+	case EDoorType::Interactive:
+		break;
+	}
+}
+
+void ADoorBase::OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+                                    AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp,
+                                    int32 OtherBodyIndex)
+{
+	if (AutoCloseDelay > 0.f && IsStateCurrent(EInteractiveActorState::Opened))
+	{
+		StartAutoClose();
+		return;
+	}
+	
+	switch (DoorType)
+	{
+	case EDoorType::Auto:
+		if (GetStateCurrent() != EInteractiveActorState::Opened) return;
+		
+		Close();
+		break;
+
+	case EDoorType::Interactive:
+		break;
+	}
+}
+
+bool ADoorBase::ProcessInteraction_Implementation(APlayerController* PlayerController)
+{
+	if (!PlayerController || DoorType != EDoorType::Interactive) return false;
+
+	if (!GetIsReversible() && GetStateCurrent() == EInteractiveActorState::Transition) return false;
+
+	StopAutoClose();
+	StartAnimation();
+	return true;
+}
+
+void ADoorBase::StartAutoClose()
+{
+	GetWorldTimerManager().SetTimer(AutoCloseDelayHandle, this, &ADoorBase::ProcessAutoClose, AutoCloseDelay, false);
+}
+
+void ADoorBase::StopAutoClose()
+{
+	if (GetWorldTimerManager().IsTimerActive(AutoCloseDelayHandle))
+	{
+		GetWorldTimerManager().ClearTimer(AutoCloseDelayHandle);
+	}
+}
+
+void ADoorBase::ProcessAutoClose()
+{
+	Close();
 }
--- Source/TrickyPrototyping/Public/Actors/InteractiveActors/DoorBase.h
+++ Source/TrickyPrototyping/Public/Actors/InteractiveActors/DoorBase.h
@@ -4,16 +4,30 @@
 
 #include "CoreMinimal.h"
 #include "Actors/InteractiveActorBase.h"
+#include "Interfaces/InteractionInterface.h"
 #include "DoorBase.generated.h"
+
+class UInteractionBoxComponent;
 
 /**
  * A base door class
  */
 
+UENUM(Blueprinttype)
+enum class EDoorType : uint8
+{
+	Manual,
+	Interactive,
+	Auto
+};
+
 UCLASS()
-class TRICKYPROTOTYPING_API ADoorBase: public AInteractiveActorBase
+class TRICKYPROTOTYPING_API ADoorBase : public AInteractiveActorBase, public IInteractionInterface
 {
 	GENERATED_BODY()
+
+public:
+	ADoorBase();
 
 protected:
 	virtual void BeginPlay() override;
@@ -21,4 +35,72 @@ protected:
 public:
 	virtual void Tick(float DeltaSeconds) override;
 
+	virtual void Disable() override;
+
+	virtual void Enable() override;
+
+protected:
+	virtual void FinishAnimation() override;
+
+	// DoorType
+public:
+	UFUNCTION(BlueprintGetter)
+	EDoorType GetDoorType() const { return DoorType; }
+
+	UFUNCTION(BlueprintSetter)
+	void SetDoorType(const EDoorType Value);
+
+private:
+	UPROPERTY(EditAnywhere,
+		BlueprintGetter=GetDoorType,
+		BlueprintSetter=SetDoorType,
+		Category="Door",
+		meta=(AllowPrivateAccess="true"))
+	EDoorType DoorType = EDoorType::Manual;
+
+	UPROPERTY(EditAnywhere,
+		BlueprintReadWrite,
+		Category="Door",
+		meta=(AllowPrivateAccess="true", EditCondition="DoorType=EDoorType::Interactive"))
+	bool bRequireLineOfSight = false;
+
+	void SwitchDoorType() const;
+
+	// Trigger
+protected:
+	UFUNCTION()
+	virtual void OnTriggerBeginOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                   AActor* OtherActor,
+	                                   UPrimitiveComponent* OtherComp,
+	                                   int32 OtherBodyIndex,
+	                                   bool bFromSweep,
+	                                   const FHitResult& SweepResult);
+
+	UFUNCTION()
+	virtual void OnTriggerEndOverlap(UPrimitiveComponent* OverlappedComponent,
+	                                 AActor* OtherActor,
+	                                 UPrimitiveComponent* OtherComp,
+	                                 int32 OtherBodyIndex);
+
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Components", meta=(AllowPrivateAccess="true"))
+	UInteractionBoxComponent* DoorTrigger = nullptr;
+
+	// Interaction
+protected:
+	virtual bool ProcessInteraction_Implementation(APlayerController* PlayerController) override;
+
+	// Auto close
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Door", meta=(ClampMint="0"))
+	float AutoCloseDelay = 2.f;
+
+	UPROPERTY(BlueprintReadOnly)
+	FTimerHandle AutoCloseDelayHandle;
+	
+	virtual void StartAutoClose();
+	
+	virtual void StopAutoClose();
+	
+	virtual void ProcessAutoClose();
 };
