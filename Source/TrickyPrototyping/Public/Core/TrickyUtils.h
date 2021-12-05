c08bcedabb92bcdfe676fb1ebefe4ab63146122a
Artyom Volkov <tricky.fat.cat@gmail.com>
2021-12-05 16:03:00 +0300

    Implement approach function

--- Source/TrickyPrototyping/Public/Core/TrickyUtils.h
+++ Source/TrickyPrototyping/Public/Core/TrickyUtils.h
@@ -6,4 +6,12 @@ class FTrickyUtils
 {
 public:
 	static bool GetPlayerViewPoint(AActor* CharacterActor, FVector& ViewLocation, FRotator& ViewRotation);
+
+	template <typename T>
+	static void Approach(T& CurrentValue, const T TargetValue, const T DeltaValue)
+	{
+		CurrentValue = TargetValue > CurrentValue
+			               ? FMath::Min(CurrentValue + DeltaValue, TargetValue)
+			               : FMath::Max(CurrentValue - DeltaValue, TargetValue);
+	}
 };
