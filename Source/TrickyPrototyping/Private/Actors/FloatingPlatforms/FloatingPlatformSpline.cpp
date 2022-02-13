03a0173aebee59f86de15a853b9be58b2d96cadc
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-02-13 20:10:35 +0300

    Create new floating platform class which moves along spline

---
+++ Source/TrickyPrototyping/Private/Actors/FloatingPlatforms/FloatingPlatformSpline.cpp
@@ -0,0 +1,81 @@
+// Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+
+#include "Actors/FloatingPlatforms/FloatingPlatformSpline.h"
+
+#include "Components/SplineComponent.h"
+#include "Core/TrickyUtils.h"
+
+void AFloatingPlatformSpline::PostInitProperties()
+{
+	if (SplineActor)
+	{
+		SplineComponent = FTrickyUtils::GetComponent<USplineComponent>(SplineActor);
+	}
+	Super::PostInitProperties();
+}
+
+void AFloatingPlatformSpline::SetSplineActor(AActor* Value)
+{
+	if (!Value) return;
+
+	SplineActor = Value;
+	SplineComponent = FTrickyUtils::GetComponent<USplineComponent>(Value);
+}
+
+void AFloatingPlatformSpline::CalculateTravelTime()
+{
+	Super::CalculateTravelTime();
+
+	if (!SplineComponent) return;
+
+	const float StartDistance = GetSplineDistance(CurrentPointIndex);
+	const float FinishDistance = GetSplineDistance(NextPointIndex);
+	const float DistanceBetweenPoints = FMath::Abs(FinishDistance - StartDistance);
+	TravelTime = DistanceBetweenPoints / Speed;
+}
+
+void AFloatingPlatformSpline::FillPointIndexes()
+{
+	if (!SplineComponent) return; // TODO Print error.
+
+	Super::FillPointIndexes();
+
+	if (bStopAtCertainPoints)
+	{
+		PointsIndexes = CustomPointsIndexes.Array();
+	}
+	else
+	{
+		for (int32 i = 0; i < SplineComponent->GetNumberOfSplinePoints(); i++)
+		{
+			PointsIndexes.Add(i);
+		}
+	}
+
+	if (SplineComponent->IsClosedLoop())
+	{
+		PointsIndexes.Add(SplineComponent->GetNumberOfSplinePoints());
+	}
+}
+
+void AFloatingPlatformSpline::MovePlatform(const float Progress)
+{
+	Super::MovePlatform(Progress);
+
+	if (!SplineComponent) return; // TODO Print error.
+
+	const float Start = GetSplineDistance(CurrentPointIndex);
+	const float Finish = GetSplineDistance(NextPointIndex);
+	const float SplinePosition = FMath::Lerp(Start, Finish, Progress);
+	const FVector NewLocation{SplineComponent->GetLocationAtDistanceAlongSpline(SplinePosition,
+																				ESplineCoordinateSpace::World)};
+	SetActorLocation(NewLocation);
+}
+
+float AFloatingPlatformSpline::GetSplineDistance(const int32 PointIndex) const
+{
+	if (!SplineComponent || !IndexIsValid(PointIndex)) return -1.f;
+
+	return SplineComponent->GetDistanceAlongSplineAtSplinePoint(PointsIndexes[PointIndex]);
+}
---
+++ Source/TrickyPrototyping/Public/Actors/FloatingPlatforms/FloatingPlatformSpline.h
@@ -0,0 +1,46 @@
+// Copyright (c) 202BeginPlay2 Artyom "Tricky Fat Cat" Volkov (tricky.fat.cat@gmail.com)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/FloatingPlatformBase.h"
+#include "FloatingPlatformSpline.generated.h"
+
+class USplineComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYPROTOTYPING_API AFloatingPlatformSpline : public AFloatingPlatformBase
+{
+	GENERATED_BODY()
+	
+protected:
+	virtual void PostInitProperties() override;
+	
+public:
+	UFUNCTION(BlueprintGetter, Category="FloatingPlatform|Spline")
+	AActor* GetSplineActor() const { return SplineActor; }
+
+	UFUNCTION(BlueprintSetter, Category="FloatingPlatform|Spline")
+	void SetSplineActor(AActor* Value);
+
+protected:
+	virtual void CalculateTravelTime() override;
+
+	virtual void FillPointIndexes() override;
+
+	virtual void MovePlatform(const float Progress) override;
+	
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="FloatingPlatform|Spline", meta=(AllowPrivateAccess="true"))
+	AActor* SplineActor = nullptr;
+
+	UPROPERTY()
+	USplineComponent* SplineComponent = nullptr;
+
+	float GetSplineDistance(const int32 PointIndex) const;
+
+protected:
+};
